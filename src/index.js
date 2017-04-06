c418e50c5f4518590c33f87e30c285e4027d3a45
Stewest <stew.west@amazeelabs.com>
2017-04-06 09:56:08 +0200

    Added third data stream for demo / deployed

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Plot from './Plot';
 
+const baseCurve = {
+  x: ["1","2","3","4","5","6","7","8","9","10","11","12","13","14"],
+  y: [0,5.225,11.555,18.94,27.46,37.585,50.95,65.95,82.72,100.78,121.33],
+  name: 'Base'
+}
 
 const storyPoints = {
   // Days of sprint
@@ -11,13 +16,16 @@ const storyPoints = {
   name: 'Story Points'
 }
 
-const baseCurve = {
-  x: ["1","2","3","4","5","6","7","8","9","10","11","12","13","14"],
-  y: [0,5.225,11.555,18.94,27.46,37.585,50.95,65.95,82.72,100.78,121.33],
-  name: 'Base'
+const demoDeployDone = {	
+  
+  // Days of sprint
+  x: ["1","2","3","4","5","6","7","8","9","10","11"],
+  // Story Points
+  y: [0, 2,9,12.5,20,32,45,60.5,78,79,97,109],
+  name: 'Demo / Deploy / Done'
 }
 
-const data = [baseCurve, storyPoints];
+const data = [baseCurve, storyPoints, demoDeployDone];
 
 ReactDOM.render(
   <div className="wrapper">
